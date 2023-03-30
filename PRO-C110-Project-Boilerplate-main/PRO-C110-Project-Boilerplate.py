18048af74e579a534b908c27f5fbb89854f9c522
ShresthShah <81873443+ShresthShah@users.noreply.github.com>
2023-03-30 14:30:23 +0530

    Add files via upload

---
+++ PRO-C110-Project-Boilerplate-main/PRO-C110-Project-Boilerplate.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+
+import tensorflow as tf
+
+model = tf.keras.models.load_model('keras_model.h5')
+
+video = cv2.VideoCapture(0)
+
+# Attaching Cam indexed as 0, with the application software
+camera = cv2.VideoCapture(0)
+
+# Infinite loop
+while True:
+
+	# Reading / Requesting a Frame from the Camera 
+	status , frame = camera.read()
+
+	# if we were sucessfully able to read the frame
+	if status:
+
+		# Flip the frame
+		frame = cv2.flip(frame , 1)
+		
+		
+		
+		#resize the frame
+		frame = cv2.resize(frame,(224,224))
+		
+		# expand the dimensions
+		
+		# normalize it before feeding to the model
+		normalised_image = frame/255.0
+		
+		# get predictions from the model
+		prediction = model.predict(normalised_image)	
+		
+		# displaying the frames captured
+		cv2.imshow('feed' , frame)
+
+		# waiting for 1ms
+		code = cv2.waitKey(1)
+		
+		# if space key is pressed, break the loop
+		if code == 32:
+			break
+
+# release the camera from the application software
+camera.release()
+
+# close the open window
+cv2.destroyAllWindows()
